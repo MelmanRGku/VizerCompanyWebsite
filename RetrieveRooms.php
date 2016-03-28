0b9ddbf0b4f978b9975a4c157a08872405e8fa9f
MelmanRGku <melmanrgku@gmail.com>
2016-03-28 14:41:48 -0600

    Temporary Load From Databse

--- RetrieveRooms.php
+++ RetrieveRooms.php
@@ -7,8 +7,8 @@ session_start();
 $projectRoot = "./";
 include_once($projectRoot."/includes/functions.php");
 
-$rooms = getListingRooms($_POST['ListingID']);
+$rooms = getListingAll($_POST['ListingID']);
 
-echo json_encode( $rooms['Items'] );
+echo json_encode( $rooms );
 
 ?>
